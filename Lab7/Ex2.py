076a60051b8423ff5bce0ca2f543d30fa0e22cda
Anton Shyshka <A.Shyshka.FIT.122.20@knute.edu.ua>
2020-11-25 21:38:28 +0200

    с

--- Lab7/Ex2.py
+++ Lab7/Ex2.py
@@ -5,5 +5,3 @@ import matplotlib.pyplot as plt
 c = Counter("каждый охотник желает знать где сидит фазан")
 plt.bar(*zip(*c.most_common()), width=.5)
 plt.show()
-
-
--- Lab7/Ex3.py
+++ Lab7/Ex3.py
@@ -3,5 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 c = Counter("Дождь идет на улице? Мы идем сейчас в парк! Дождь идет на улице... О чем вы тут шушукаетесь?")
+
+def charCounter(validChars):
+       vc = set(validChars)
+       def counter(ln):
+           return sum(1 for ch in ln if ch in vc)
+       return counter
+countSentences = charCounter('...!?')
 plt.bar(*zip(*c.most_common()), width=.5)
 plt.show()
